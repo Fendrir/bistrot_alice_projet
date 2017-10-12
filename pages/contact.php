3a878cf9ae93c359ca841957a6cca0429d2138d7
isam14 <imuck14@gmail.com>
2017-10-12 21:41:02 +0200

    modif col

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,5 @@
 
-<div class="col-6 card card-body">
+<div class="col-md-9 card card-body">
 <p class="card-text"><h1>Le Bistrot d'Alice</h1></p>
 <p class="card-text">26 rue Chartrand</p>
 <p class="card-text">11000 Carcassonne</p>
@@ -10,10 +10,10 @@
 <p class="card-text">Dimanche et Lundi : Fermé</p>
 <p class="card-text">info@lebistrotdalice.fr</p>
 
-<p class="card-text">
+
 <iframe class="img-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2907.852566291363!2d2.349163450494714!3d43.212581579036446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ae2c375dfb3d17%3A0x4c0b247405732500!2s26+Rue+Chartran%2C+11000+Carcassonne!5e0!3m2!1sfr!2sfr!4v1507654618207"
     width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </p>
+   
     </div>
     
     
