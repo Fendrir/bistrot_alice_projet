aab0849aa3c51aae50cee276f6907b2a2c326361
isam14 <imuck14@gmail.com>
2017-10-17 15:44:53 +0200

    layout ok

--- pages/contact.php
+++ pages/contact.php
@@ -1,32 +1,26 @@
 <div class="row">
-<div class="col-md-6 text-center">
-
-
-    
-    <h1>Le Bistrot d'Alice</h1><br>
-    <address>26 rue Chartrand<br>
-        11000 Carcassonne<br>
-        04 68 47 25 51<br>
-    </address>
-    <p>
-        Réservations UNIQUEMENT par téléphone
-    </p> 
-    <ul>
-        <li>Horaires :</li>
-        <li>Du mardi au Samedi : 12H-14H30 / 19H30-22H00</li>
-        <li>Dimanche et Lundi : Fermé</li>
-
-    </ul>  
-    
-    <p>
-        info@lebistrotdalice.fr
-    </p>  
-</div>
-<div class="col-md-6">
-    <iframe class="img-fluid map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11631.410184879449!2d2.351357!3d43.212582!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ae2c375dfb3d17%3A0x4c0b247405732500!2s26+Rue+Chartran%2C+11000+Carcassonne%2C+France!5e0!3m2!1sfr!2sfr!4v1507839006307" width="auto" height="800" frameborder="0" style="border:0" allowfullscreen></iframe>
-
-</div>
+    <div class="col-md-6 text-center">
+        <h1>Le Bistrot d'Alice</h1><br>
+        <address>26 rue Chartrand<br>
+            11000 Carcassonne<br>
+            04 68 47 25 51<br>
+        </address>
+        <p>
+            Réservations UNIQUEMENT par téléphone
+        </p>
+        <ul>
+            <li>Horaires :</li>
+            <li>Du mardi au Samedi : 12H-14H30 / 19H30-22H00</li>
+            <li>Dimanche et Lundi : Fermé</li>
+        </ul>
+        <p>
+            info@lebistrotdalice.fr
+        </p>
     </div>
+    <div class="col-md-6">
+        <iframe class="img-fluid map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11631.410184879449!2d2.351357!3d43.212582!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ae2c375dfb3d17%3A0x4c0b247405732500!2s26+Rue+Chartran%2C+11000+Carcassonne%2C+France!5e0!3m2!1sfr!2sfr!4v1507839006307" width="auto" height="800" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+</div>
 
 
 
