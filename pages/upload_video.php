bcf64244b238422c7566cfddd443378c57f62a96
Rdyx <tiranico.pro@gmail.com>
2017-10-18 11:10:21 +0200

    video link system + preparation hovers index

---
+++ pages/upload_video.php
@@ -0,0 +1,28 @@
+<?php
+if(!isset($_SESSION['nick']) || $_SESSION['nick'] !== 'Franck') {
+    echo 'erreur lolilol';
+
+} else {
+    $videoLink = $_POST['videoLink'];
+    $sql = "UPDATE video SET vid_link = '$videoLink' WHERE vid_oid = 1";
+    $result = $conn->query($sql);
+        ?>
+
+        <!-- Résumé du post -->
+        <div class="col-9 align-self-center text-center">
+            <h1>Vidéo ajoutée avec succès !</h1>
+            <div class="col mt-5">
+                <iframe class="taillevideo-admin text-center" src="https://www.youtube.com/embed/<?= $videoLink ?>" frameborder="0"
+                        allowfullscreen></iframe>
+            </div>
+            <div class="mt-3">
+                <a href="?p=administration">
+                    <button class="btn btn-info">Retour à la page d'administration</button>
+                </a>
+            </div>
+        </div>
+        <!-- FIN Résumé du post -->
+
+        <?php
+}
+?>
